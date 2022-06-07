7f01830f1b246ec5cb3533104581f998d6ff9308
khalyomede <khalyomede@gmail.com>
2022-06-07 22:45:57 +0200

    fix test

--- tests/database/factories/CartFactory.php
+++ tests/database/factories/CartFactory.php
@@ -18,7 +18,7 @@ final class CartFactory extends Factory
     public function definition(): array
     {
         return [
-            'name' => $this->faker->name(),
+            'name' => $this->faker->word(),
         ];
     }
 }
